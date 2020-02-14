f56c1b071d5ac5006e3154b6941d0873857aea74
stepanenko <serhiy.stepanenko@aiesec.net>
2020-02-14 23:53:02 +0200

    cleaned about

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,13 +5,3 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae temporibus doloremque quae praesentium tenetur. Delectus voluptas suscipit tempora quisquam repudiandae soluta alias nemo, exercitationem quaerat non, at itaque autem corrupti saepe error magni ad aliquid animi temporibus. Ipsam, impedit ducimus?</p>
   </div>
 </template>
-
-<script>
-  export default {
-    name: 'About'
-  };
-</script>
-
-<style scoped>
-
-</style>
