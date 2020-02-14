f77824d2b2ed6b4f5e774821e34a20f3469c559b
stepanenko <serhiy.stepanenko@aiesec.net>
2020-02-14 23:42:04 +0200

    added routing

--- src/App.vue
+++ src/App.vue
@@ -2,51 +2,17 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" class="add-todo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"></Todos>
+    <router-view/>
   </div>
 </template>
 
 <script>
 import Header from './components/layout/Header';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-import axios from 'axios';
 
 export default {
   name: 'app',
   components: {
-    Header,
-    Todos,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: []
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-        .then(() => this.todos = this.todos.filter(todo => todo.id !== id))
-        .catch(err => console.log(err));
-    },
-
-    addTodo(newTodo) {
-      const { title, completed } = newTodo;
-
-      axios.post('https://jsonplaceholder.typicode.com/todos', {
-        title,
-        completed
-      })
-        .then(res => this.todos = [...this.todos, res.data])
-        .catch(err => console.log(err));
-    }
-  },
-  created() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
-      .then(res => this.todos = res.data)
-      .catch(err => console.log(err));
+    Header
   }
 }
 </script>
@@ -55,24 +21,5 @@ export default {
   * {
     box-sizing: border-box;
     margin: 0;
-  }
-  .add-todo {
-    height: 43px;
-  }
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-    padding: 0 20%;
-  }
-  .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
-  .btn:hover {
-    background: #666;
   }
 </style>
